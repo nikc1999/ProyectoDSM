6dd642d06407cec0e56960a780fc0faee529894c
nikc1999 <nikc987@gmail.com>
2022-10-29 10:06:16 -0300

    Creacion nuevo main.py para API y cambio nombre del antigo main.py a testAPI.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI #FastAPI
+import sqlite3  #base de datos
+
+from pydantic import BaseModel #Creacion Modelos
+from typing import Text, Optional #Tipos de datos y extras para Modelos
+from uuid import uuid4 as uuid #Generar Id aleatorio Unico
+
